f58d41d876891cf7d3726faa76a53bae199b2f61
Matt <m.russell6@students.clark.edu>
2019-04-13 23:11:02 -0700

    two modified scripts to test workflow assignment.

---
+++ helloModified.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# hello.py
+##making this executable bash script for my linux environment
+
+def main():
+    print("where's the beef?")
+def function():    
+    print("Hello, Bill")
+def matt():
+    print("Matt is here to learn python")
+main()    
+function()
+matt()
+
+##Made executable by adding shebang in line 1
+##executed chmod +x helloModified.py
+##executed by running ./ before script file name
---
+++ myFileModified.py
@@ -0,0 +1,8 @@
+## Testing the commit and push to my repository
+def function():
+    print("this is another function")
+def main():
+    print("this is my function")
+
+main()
+function()
